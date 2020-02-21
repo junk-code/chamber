8f5642dd54b47bfb8bb8573bc40fa9869c4aad88
Victor Lawrence <victor.lawrence@uga.edu>
2020-02-21 10:45:06 -0500

    removed a dumb meaningless error message and added the .jsx extension to a file.

--- src/client.js
+++ src/client.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { App } from './App/components/App.js'
-
-console.error(`Hello I'm a problem!`)
+import { App } from './App/components/App.jsx'
 
 render(<App />, document.getElementById('root'))
